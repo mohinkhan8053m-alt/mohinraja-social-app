d0809c25200b06c10e35caf4a56005482cbc991d
mohinkhan8053m-alt <mohinkhan8053m@gmail.com>
2026-06-09 22:21:43 +0530

    Refactor main.jsx by cleaning up comments
    Removed comments in the main.jsx file for clarity.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // 1. यह जरूरी है
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css'; // अगर आप टेलविंड इस्तेमाल कर रहे हैं
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* 2. इसे यहाँ लपेटना जरूरी है ताकि नेविगेशन काम करे */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
