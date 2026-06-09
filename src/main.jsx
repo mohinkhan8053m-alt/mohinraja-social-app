313d7fa36185c2972c96621824b1be76c19d3269
mohinkhan8053m-alt <mohinkhan8053m@gmail.com>
2026-06-09 21:32:47 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 1. यह जरूरी है
 import App from './App.jsx';
+import './index.css'; // अगर आप टेलविंड इस्तेमाल कर रहे हैं
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* 2. इसे यहाँ लपेटना जरूरी है ताकि नेविगेशन काम करे */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
