6f1bdf9f90b033b0b3e1e324c5edb3739bd168d8
mohinkhan8053m-alt <mohinkhan8053m@gmail.com>
2026-06-13 00:35:36 +0530

    Update UserProvider import to UserContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { UserProvider } from './UserProvider.jsx'; // अपना यूजर डेटा यहाँ जोड़ें
+import { UserProvider } from './UserContext.jsx'; // यहाँ नाम सही कर दिया है
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* अब पूरा ऐप यूजर डेटा को पहचान पाएगा */}
     <UserProvider>
       <App />
     </UserProvider>
