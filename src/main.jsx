1269df01f7ed5d9ce738c8533f55ff7ae50987e7
mohinkhan8053m-alt <mohinkhan8053m@gmail.com>
2026-06-13 00:00:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { UserProvider } from './UserProvider.jsx'; // अपना यूजर डेटा यहाँ जोड़ें
 
-// यह फाइल App को रेंडर करती है
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* अब पूरा ऐप यूजर डेटा को पहचान पाएगा */}
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
