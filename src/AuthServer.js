39c87e61a498c56dfb12b5baab639a126fefa969
mohinkhan8053m-alt <mohinkhan8053m@gmail.com>
2026-06-18 14:00:45 +0530

    Create AuthServer.js

---
+++ src/AuthServer.js
@@ -0,0 +1,5 @@
+// यहाँ यूजर की पहचान का कोड आएगा
+export const loginUser = (email, password) => {
+    // Supabase या Backend auth कॉल यहाँ आएगा
+    return { status: "success", user: "Mohin Raja" };
+};
