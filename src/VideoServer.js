ea1987fe2f37b217cc8da2c0fe0f1eb7d0146567
mohinkhan8053m-alt <mohinkhan8053m@gmail.com>
2026-06-18 14:00:16 +0530

    Create VideoServer.js

---
+++ src/VideoServer.js
@@ -0,0 +1,5 @@
+// यह फाइल सिर्फ VideoCallHub, ProVideoCall, आदि के लिए है
+export const initiateVideoCall = (userA, userB) => {
+    console.log("Video Call initiated between", userA, userB);
+    // यहाँ WebRTC का सिग्नलिंग कोड आएगा
+};
