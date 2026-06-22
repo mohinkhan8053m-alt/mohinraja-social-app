87a7c79ce44f4f9b0c16ad6144179b2b4faa741a
mohinkhan8053m-alt <mohinkhan8053m@gmail.com>
2026-06-22 22:25:48 +0530

    Create PricingLogic.js

---
+++ src/PricingLogic.js
@@ -0,0 +1,10 @@
+// PricingLogic.js
+export const getCountryPricing = (country) => {
+    // यहाँ अपना वो लॉजिक डालें जो आप प्रीमियम प्राइसिंग के लिए यूज़ करते हैं
+    // अभी के लिए एक डिफॉल्ट रिटर्न दे रहे हैं
+    return {
+        price: 99, 
+        currency: 'INR',
+        plan: 'Premium'
+    };
+};
